3882175503b65428567e100ff3a16e26be02fda4
Prasannjeet Singh <1756270+prasannjeet@users.noreply.github.com>
2023-08-02 12:04:00 +0200

    Add function to save downloaded files for analysis

--- analyse.py
+++ analyse.py
@@ -2,6 +2,9 @@ import os
 import requests
 from dotenv import load_dotenv
 from urllib.parse import urlencode
+import json
+from dataclasses import dataclass
+from typing import List
 
 
 class AudioData:
@@ -11,6 +14,16 @@ class AudioData:
         self.start = start
         self.audioBlob = requests.get(audioUrl).content
 
+    def __str__(self):
+        return f"AudioData(questionId={self.questionId}, audioUrl={self.audioUrl}, start={self.start})"
+
+    def to_dict(self):
+        return {
+            "questionId": self.questionId,
+            "audioUrl": self.audioUrl,
+            "start": self.start
+        }
+
 
 class UserData:
     def __init__(self, userId, audio, fifUrl, fifStartTime):
@@ -19,6 +32,18 @@ class UserData:
         self.fifUrl = fifUrl
         self.fifStartTime = fifStartTime
         self.fifFileBlob = requests.get(fifUrl).content
+
+    def __str__(self):
+        audio_str = ', '.join(str(a) for a in self.audio)
+        return f"UserData(userId={self.userId}, audio=[{audio_str}], fifUrl={self.fifUrl}, fifStartTime={self.fifStartTime})"
+
+    def to_dict(self):
+        return {
+            "userId": self.userId,
+            "audio": [a.to_dict() for a in self.audio],
+            "fifUrl": self.fifUrl,
+            "fifStartTime": self.fifStartTime
+        }
 
 
 def get_access_token():
@@ -40,10 +65,29 @@ def get_user_data(token):
     return [UserData(**user_data) for user_data in response.json()]
 
 
+def save_files(user_data: UserData):
+    # Save FIF file
+    with open(f"{user_data.userId}.fif", "wb") as fif_file:
+        fif_file.write(user_data.fifFileBlob)
+
+    # Save audio files
+    for audio_data in user_data.audio:
+        with open(f"{user_data.userId}_{audio_data.questionId}.mp3", "wb") as audio_file:
+            audio_file.write(audio_data.audioBlob)
+
+
 def main():
     load_dotenv()
     token = get_access_token()
     user_data_list = get_user_data(token)
+
+    # Save files for each user data
+    for user_data in user_data_list:
+        save_files(user_data)
+
+    user_data_dicts = [user_data.to_dict() for user_data in user_data_list]
+    pretty_json = json.dumps(user_data_dicts, indent=4)
+    print(pretty_json)
     print("Done")
 
 
